387c66b01713bc9c3c7fac76856f0998dcc7d4e2
muhammedezgin <muhammedezgin47@gmail.com>
2022-07-19 17:08:26 +0300

    angular-web-ornek

---
+++ intro/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ProductComponent } from './product/product.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'intro';
+  firstName = 'ezgin'
+  
+}
+
+
+
+
+
---
+++ shop/src/app/login/login.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable, NgModule } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { Observable } from "rxjs";
+import { AccountService } from "../services/account.service";
+
+
+@Injectable()
+@NgModule()
+export class LoginGuard implements CanActivate {
+
+    constructor(private accountService: AccountService,
+        private router: Router) { }
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        let logged = this.accountService.isLoggedIn();
+        if (logged) {
+            return true;
+        }
+        this.router.navigate(["login"]);
+        return false;
+    }
+
+}
